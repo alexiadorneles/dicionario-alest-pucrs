aaa68e51050728f03a366d1747e41594e1a7b2a7
alexiadorneles <lecavsalexia@gmail.com>
2020-06-26 01:32:19 -0300

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.insert("Bala");
-        trie.insert("Bola");
+        trie.insert("Bar");
+        trie.insert("Baia");
         trie.insert("Baiano");
-        trie.insert("Alexia");
+        trie.insert("Bola");
+        trie.insert("Boia");
 
-        System.out.println(trie);
+        System.out.println("Digite o início da palavra");
+        LinkedList<String> characters = trie.positionsPre();
+//        trie.positionsPos();
+        System.out.println();
     }
 }
--- src/main/Trie.java
+++ src/main/Trie.java
@@ -1,12 +1,36 @@
 package main;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 class Trie {
+    private TrieNode last;
+
     private class TrieNode {
         private final Map<Character, TrieNode> children = new HashMap<>();
         private boolean endOfWord;
+        private Character element;
+        private TrieNode parent;
+
+        public TrieNode(Character element) {
+            this.element = element;
+        }
+
+        public TrieNode() {
+        }
+
+        public void setParent(TrieNode parent) {
+            this.parent = parent;
+        }
+
+        public TrieNode getParent() {
+            return parent;
+        }
 
         Map<Character, TrieNode> getChildren() {
             return children;
@@ -30,8 +54,25 @@ class Trie {
     void insert(String word) {
         TrieNode current = root;
 
+//        char[] chars = word.toCharArray();
+////        for (int i = 0; i < chars.length; i++) {
+//////            char parent = i > 0 ? chars[i-1] :
+////            TrieNode finalCurrent = current;
+////            current = current.getChildren().computeIfAbsent(chars[i], (c) -> {
+////                TrieNode node = new TrieNode(c);
+////                node.setParent(finalCurrent);
+////                return node;
+////            });
+////        }
+////
         for (char l : word.toCharArray()) {
-            current = current.getChildren().computeIfAbsent(l, c -> new TrieNode());
+            TrieNode finalCurrent1 = current;
+            current = current.getChildren().computeIfAbsent(l, c -> {
+                TrieNode node = new TrieNode(c);
+                node.setParent(finalCurrent1);
+                return node;
+            });
+
         }
         current.setEndOfWord(true);
     }
@@ -78,5 +119,92 @@ class Trie {
             return current.getChildren().isEmpty();
         }
         return false;
+    }
+
+    public LinkedList<String> positionsPre() {
+        LinkedList<String> lista = new LinkedList<>();
+        LinkedList<String> res = new LinkedList<>();
+        StringBuilder a = new StringBuilder();
+        positionsPreAux(root, lista, a, res, root.parent);
+        return res;
+    }
+
+    private void positionsPreAux(TrieNode n, LinkedList<String> lista, StringBuilder builder, List<String> res, TrieNode previous) { // recursao
+        if (n != null) {
+            if (n.element != null) {
+                lista.add(String.valueOf(n.element));
+            }
+            StringBuilder finalBuilder = extractDataFromParent(n, builder, previous);
+            n.getChildren().forEach((key, value) -> {
+                positionsPreAux(value, lista, finalBuilder, res, n);
+                if (value.isEndOfWord()) {
+                    this.addThingsFromBuilder(lista, finalBuilder);
+                    res.addAll(lista);
+                    res.add("-----------");
+                    lista.clear();
+                }
+            });
+        }
+    }
+
+    private StringBuilder extractDataFromParent(TrieNode n, StringBuilder builder, TrieNode previous) {
+        TrieNode parent = n.parent != null ? n.parent.parent : n;
+        if (parent != null && parent.isEndOfWord()
+                || (
+                builder.length() == 0 && n.parent != null
+        )) {
+            do {
+                if (parent != null) {
+                    parent = parent.getParent();
+                    if (parent != null && parent.element != null) {
+                        builder.append(parent.element);
+                    }
+                }
+            } while (parent != null);
+            builder = builder.reverse();
+        } else {
+            builder = new StringBuilder();
+        }
+        return builder;
+    }
+
+    private void addThingsFromBuilder(LinkedList<String> lista, StringBuilder finalBuilder) {
+        if (finalBuilder.length() > 0) {
+            char[] chars = finalBuilder.toString().toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                lista.add(i, String.valueOf(chars[i]));
+            }
+        }
+    }
+
+    public LinkedList<String> positionsPos() {
+        LinkedList<String> lista = new LinkedList<>();
+        StringBuilder builder = new StringBuilder();
+        positionsPosAux(root, lista, builder);
+        return lista;
+    }
+
+    private void positionsPosAux(TrieNode n, LinkedList<String> lista, StringBuilder builder) {
+        TrieNode parent = n;
+        if (n != null) {
+            if (n.isEndOfWord()) {
+                builder.append(n.element);
+                do {
+                    parent = parent.getParent();
+                    if (parent != null && parent.element != null) {
+                        builder.append(parent.element);
+                    }
+                } while (parent != null);
+            }
+
+            String text = builder.reverse().toString();
+            if (!text.isEmpty()) {
+                lista.add(text);
+            }
+
+            n.getChildren().forEach((key, value) -> {
+                positionsPosAux(value, lista, new StringBuilder());
+            });
+        }
     }
 }
